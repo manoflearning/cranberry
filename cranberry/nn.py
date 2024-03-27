95fcdd431a4652661e576a70a3c3fed37c70a183
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-03-28 05:07:03 +0900

    minor changes

--- cranberry/nn.py
+++ cranberry/nn.py
@@ -5,7 +5,7 @@ class Linear:
     def __init__(self, in_features: int, out_features: int, bias=True):
         self.weight = Tensor.kaiming_uniform(shape=[out_features, in_features], a=math.sqrt(5))
         bound = 1 / math.sqrt(in_features)
-        self.bias = Tensor.uniform(shape=[out_features, in_features], low=-bound, high=bound) if bias else None
+        self.bias = Tensor.uniform(shape=[in_features], low=-bound, high=bound) if bias else None
 
     def __call__(self, x: Tensor) -> Tensor:
         return x.linear(weight=self.weight, bias=self.bias)
