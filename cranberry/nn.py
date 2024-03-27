adf3f5a54b84a741839d7078727f77adbcedf41c
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-03-28 02:51:07 +0900

    cranberry.nn

--- cranberry/nn.py
+++ cranberry/nn.py
@@ -2,12 +2,14 @@ import math
 from cranberry import Tensor
 
 class Linear:
-
     def __init__(self, in_features: int, out_features: int, bias=True):
-        super().__init__()
-        self.weight = Tensor.kaiming_uniform([out_features, in_features], math.sqrt(5))
+        self.weight = Tensor.kaiming_uniform(shape=[out_features, in_features], a=math.sqrt(5))
         bound = 1 / math.sqrt(in_features)
-        self.bias = Tensor.uniform([out_features], -bound, bound) if bias else None
+        self.bias = Tensor.uniform(shape=[out_features, in_features], low=-bound, high=bound) if bias else None
 
     def __call__(self, x: Tensor) -> Tensor:
-        return x.linear(self.weight, self.bias)
+        return x.linear(weight=self.weight, bias=self.bias)
+
+class ReLU:
+    def __call__(self, x: Tensor) -> Tensor:
+        return x.relu()
