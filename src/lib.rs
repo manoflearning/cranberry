4cd3cad7ecb0e6b1e759e2d897c3e7dcf0a3e395
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-04-01 21:44:08 +0900

    change thread priority..?

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(portable_simd)]
 use pyo3::prelude::*;
+use thread_priority::*;
 
 mod tensor;
 use tensor::Tensor;
@@ -7,6 +8,7 @@ mod data;
 
 #[pymodule]
 fn cranberry(_py: Python, m: &PyModule) -> PyResult<()> {
+    assert!(set_current_thread_priority(ThreadPriority::Min).is_ok());
     m.add_class::<Tensor>()?;
     Ok(())
 }
