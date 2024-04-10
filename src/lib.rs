bbe1bbe42fbac96d9c88986724452803b0d5c530
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-04-10 16:57:04 +0900

    create optim (sgd)

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,13 @@ use thread_priority::*;
 mod tensor;
 use tensor::Tensor;
 mod data;
+mod optim;
+use optim::SGD;
 
 #[pymodule]
 fn cranberry(_py: Python, m: &PyModule) -> PyResult<()> {
     assert!(set_current_thread_priority(ThreadPriority::Min).is_ok());
     m.add_class::<Tensor>()?;
+    m.add_class::<SGD>()?;
     Ok(())
 }
---
+++ src/optim.rs
@@ -0,0 +1,28 @@
+use crate::tensor::Tensor;
+use pyo3::prelude::*;
+
+#[pyclass]
+pub struct SGD {
+    params: Vec<Tensor>,
+    lr: f32,
+}
+
+#[pymethods]
+impl SGD {
+    #[new]
+    fn new(params: Vec<Tensor>, lr: f32) -> PyResult<Self> {
+        Ok(Self { params, lr })
+    }
+
+    fn zero_grad(&self) {
+        for param in &self.params {
+            param.zero_grad_();
+        }
+    }
+
+    fn step(&self) {
+        for param in &self.params {
+            param.step_(self.lr);
+        }
+    }
+}
