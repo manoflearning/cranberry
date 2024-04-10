1a4180c4777c4aafe836d90df94baac3aa3e8157
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-04-10 10:31:17 +0900

    more test for tensor

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -2,6 +2,12 @@ import cranberry as cr
 import numpy as np
 import unittest
 
+np.random.seed(1337)
+
+A_np = np.random.randn(3, 3).astype(np.float32)
+B_np = np.random.randn(3, 3).astype(np.float32)
+C_np = np.random.randn(1, 3).astype(np.float32)
+
 class TestCranberry(unittest.TestCase):
     def test_zerodim_initialization(self):
         a = cr.Tensor(55)
@@ -11,26 +17,20 @@ class TestCranberry(unittest.TestCase):
         self.assertEqual(a.shape, [])
         self.assertEqual(b.shape, [])
 
-    def test_plus_equals(self):
-        a = cr.Tensor.uniform([10, 10], -100, 100)
-        b = cr.Tensor.uniform([10, 10], -100, 100)
+    def test_add(self):
+        A = cr.Tensor(A_np)
+        B = cr.Tensor(B_np)
+        np.testing.assert_allclose(A_np + B_np, (A + B).numpy())
 
-        c = a + b
-        val1 = np.array(c.data)
-        a += b
-        val2 = np.array(a.data)
+    def test_add_broadcast(self):
+        A = cr.Tensor(A_np)
+        C = cr.Tensor(C_np)
+        np.testing.assert_allclose(A_np + C_np, (A + C).numpy())
 
-        np.testing.assert_allclose(val1, val2)
+    def test_matmul(self):
+        A = cr.Tensor(A_np)
+        B = cr.Tensor(B_np)
+        np.testing.assert_allclose(A_np @ B_np, (A @ B).numpy(), rtol=1e-7, atol=1e-7)
 
-    def test_matmul_equals(self):
-        a = np.random.rand(2, 2).astype(np.float32)
-        b = np.random.rand(2, 2).astype(np.float32)
-        c = a @ b
-
-        p = cr.Tensor(a)
-        q = cr.Tensor(b)
-        r = p @ q
-
-        
 if __name__ == '__main__':
     unittest.main()
