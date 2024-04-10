fix backprop bug

test/test_tensor.py
@@ -1,12 +1,15 @@
-import cranberry as cr
 import numpy as np
+import cranberry as cr
+import torch
 import unittest
 
 np.random.seed(1337)
 
 A_np = np.random.randn(3, 3).astype(np.float32)
 B_np = np.random.randn(3, 3).astype(np.float32)
-C_np = np.random.randn(1, 3).astype(np.float32)
+C_np = np.random.randn(3, 3).astype(np.float32)
+
+Z_np = np.random.randn(1, 3).astype(np.float32)
 
 class TestCranberry(unittest.TestCase):
     def test_zerodim_initialization(self):
@@ -24,13 +27,45 @@ class TestCranberry(unittest.TestCase):
 
     def test_add_broadcast(self):
         A = cr.Tensor(A_np)
-        C = cr.Tensor(C_np)
-        np.testing.assert_allclose(A_np + C_np, (A + C).numpy())
+        Z = cr.Tensor(Z_np)
+        np.testing.assert_allclose(A_np + Z_np, (A + Z).numpy())
 
     def test_matmul(self):
         A = cr.Tensor(A_np)
         B = cr.Tensor(B_np)
         np.testing.assert_allclose(A_np @ B_np, (A @ B).numpy(), rtol=1e-7, atol=1e-7)
+
+    def test_backward_pass(self):
+        def run_cranberry():
+            A = cr.Tensor(A_np, requires_grad=True)
+            B = cr.Tensor(B_np, requires_grad=True)
+            C = cr.Tensor(C_np)
+            Z = cr.Tensor(Z_np)
+            out = A.matmul(B)
+            out = out.relu()
+            out = out * C
+            out = out - Z
+            out = out.pow(4)
+            out = out.sum()
+            out.backward()
+            return out.numpy(), A.grad, B.grad
+        
+        def run_pytorch():
+            A = torch.tensor(A_np, requires_grad=True)
+            B = torch.tensor(B_np, requires_grad=True)
+            C = torch.tensor(C_np)
+            Z = torch.tensor(Z_np)
+            out = A.matmul(B)
+            out = out.relu()
+            out = out * C
+            out = out - Z
+            out = out.pow(4)
+            out = out.sum()
+            out.backward()
+            return out.detach().numpy(), A.grad, B.grad
+
+        for x, y in zip(run_cranberry(), run_pytorch()):
+            np.testing.assert_allclose(x, y, rtol=1e-7, atol=1e-7)
 
 if __name__ == '__main__':
     unittest.main()