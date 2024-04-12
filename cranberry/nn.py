a467a6ca56733c3eb79255fbdc6c49647cf90124
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-04-13 00:21:32 +0900

    change nn.Linear

--- cranberry/nn.py
+++ cranberry/nn.py
@@ -2,13 +2,14 @@ import math
 import cranberry as cb
 
 class Linear:
+    # https://github.com/tinygrad/tinygrad/blob/master/tinygrad/nn/__init__.py#L72-L80
     def __init__(self, in_features: int, out_features: int, bias=True):
-        self.weight = cb.Tensor.kaiming_uniform(shape=[in_features, out_features], a=math.sqrt(5))
+        self.weight = cb.Tensor.kaiming_uniform(shape=[out_features, in_features], a=math.sqrt(5))
         bound = 1 / math.sqrt(in_features)
         self.bias = cb.Tensor.uniform(shape=[out_features], low=-bound, high=bound) if bias else None
 
     def __call__(self, x: cb.Tensor) -> cb.Tensor:
-        return x.linear(weight=self.weight, bias=self.bias)
+        return x.linear(weight=self.weight.transpose(0, 1), bias=self.bias)
 
     def __str__(self):
         return f"Linear(in_features={self.weight.shape[0]}, out_features={self.weight.shape[1]}, bias={self.bias is not None})"        
