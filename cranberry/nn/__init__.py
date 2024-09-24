fe8899043063ad2558784b6bbcbfd5986982b9b2
Wookyung Jeong <77jwk0724@gmail.com>
2024-09-24 20:02:16 +0900

    style: python format style change, and minor refactoring (#52)

--- cranberry/nn/__init__.py
+++ cranberry/nn/__init__.py
@@ -7,20 +7,16 @@ from typing import List
 
 class Module:
   @abstractmethod
-  def __call__(self, x: Tensor) -> Tensor:
-    pass
+  def __call__(self, x: Tensor) -> Tensor: pass
 
   @abstractmethod
-  def parameters(self) -> List[Tensor]:
-    pass
+  def parameters(self) -> List[Tensor]: pass
 
 
 class ReLU(Module):
-  def __call__(self, x: Tensor) -> Tensor:
-    return x.relu()
+  def __call__(self, x: Tensor) -> Tensor: return x.relu()
 
-  def parameters(self) -> List[Tensor]:
-    return []
+  def parameters(self) -> List[Tensor]: return []
 
 
 class Linear(Module):
@@ -42,8 +38,7 @@ class Sequential(Module):
     self.layers = layers
 
   def __call__(self, x: Tensor) -> Tensor:
-    for layer in self.layers:
-      x = layer(x)
+    for layer in self.layers: x = layer(x)
     return x
 
   def parameters(self) -> List[Tensor]:
